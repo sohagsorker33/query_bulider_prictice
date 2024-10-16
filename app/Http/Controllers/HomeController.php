dae472d1cad73fd24019650e0bf15128f101fe84
Ashraful Islam Sohag <sohagali09876543@gmail.com>
2024-10-16 19:11:32 +0600

    thrid comit

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -11,8 +11,55 @@ class HomeController extends Controller
   {
     //selection all rows
    // $result=DB::table('brands')->get();
+
   // selection by first row
-   $result=DB::table('brands')->first();
+   //$result=DB::table('brands')->first();
+
+   // selection by find  row
+  // $result=DB::table('brands')->find(3);
+
+  // selecting specific single column
+  //$result=DB::table('brands')->pluck('brandName');
+
+  //row count
+  //$result=DB::table('products')->count();
+
+  // max(), min(), avg(), sum()
+  //$result=DB::table('products')->sum('price');
+
+    //selection specific multiple  column
+   //$result=DB::table('products')->select('title','price')->get();
+
+   //select unique
+ // $result=DB::table('products')->select('title')->distinct()->get();
+
+ // Inner Join
+ /* $result=DB::table('products')
+ ->join('categories','products.category_id', '=', 'categories.id')
+ ->join('brands','products.brand_id', 'brands.id')
+ ->get(); */
+
+
+ //left join
+/*  $result=DB::table('products')
+ ->leftJoin('categories','products.category_id', '=', 'categories.id')
+ ->leftJoin('brands','products.brand_id', 'brands.id')
+ ->get(); */
+
+ // right join
+/*  $result=DB::table('products')
+ ->rightJoin('categories','products.category_id', '=', 'categories.id')
+ ->rightJoin('brands','products.brand_id', 'brands.id')
+ ->get();
+ */
+
+ // cross join
+
+$result=DB::table('products')
+->crossJoin('categories')
+->crossJoin('brands')
+->get();
     return $result;
+
   }
 }
